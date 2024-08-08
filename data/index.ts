37ea8443381a7cba7befa67ed4f80b45f0eb9e18
himanshukandari14 <himanshukandari14@gmail.com>
2024-08-08 21:30:19 +0530

    social media added

--- data/index.ts
+++ data/index.ts
@@ -165,13 +165,12 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
+    link: "https://github.com/himanshukandari14",
   },
-  {
-    id: 2,
-    img: "/twit.svg",
-  },
+
   {
     id: 3,
     img: "/link.svg",
+    link: "https://www.linkedin.com/in/himanshu-kandari-874a57230/",
   },
 ];
